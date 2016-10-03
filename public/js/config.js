1a0cc1b1d48f0808a54a80e5c1029b11d68b3e24
Tom Lewis <grimsheep@gmail.com>
2016-10-03 17:10:15 +0100

    Added teamspeak and stats.piratemc.com moitors

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,9 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
   'm776305635-e64b69ee1f2f309809c5819b', // Website
-  'm777382274-e66e7c900ecd2bb6732a9306' // Main Server
+  'm777382274-e66e7c900ecd2bb6732a9306', // Main Server
+	'm776511954-b3079981db189bc0d113584f', // stats.piratemc.com HTTP check
+	'm778221920-414b82938a2d76da86bd72dd' // Teamspeak port check
 ];
 
 // refresh interval (in seconds)
